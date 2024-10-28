5183d26443e59d7520ecdc517ad580864ef059bc
Oleksandr <oleksandr.danyliuk.22@pnu.edu.ua>
2024-10-28 23:39:48 +0200

    add blueprint and test_user_bp

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-
 app = Flask(__name__)
-app.config.from_pyfile("../config.py")
+app.config.from_pyfile('../config.py')
 
-from . import views
-
-from .posts import post_bp
+from . import views  # noqa: E402, F401
+from app.posts import post_bp  # noqa: E402, F401
+from .users import user_bp  # noqa: E402, F401
 app.register_blueprint(post_bp)
+app.register_blueprint(user_bp)
