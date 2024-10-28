2b94daf2f489521585dd30777bd5660e499476a7
Oleksandr <oleksandr.danyliuk.22@pnu.edu.ua>
2024-10-28 20:34:34 +0200

    add blueprint

--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
 from flask import  request, redirect, url_for, render_template, abort
 from . import app
 
-
 @app.route('/')
 def main():
     return render_template("base.html")
@@ -12,6 +11,7 @@ def home():
     agent = request.user_agent
 
     return render_template("home.html", agent=agent)
+
 
 @app.route("/hi/<string:name>")   #/hi/ivan?age=45&q=fdfdf
 def greetings(name):
@@ -27,23 +27,8 @@ def admin():
     print(to_url)
     return redirect(to_url)
 
-posts = [
-    {"id": 1, 'title': 'My First Post', 'content': 'This is the content of my first post.', 'author': 'John Doe'},
-    {"id": 2, 'title': 'Another Day', 'content': 'Today I learned about Flask macros.', 'author': 'Jane Smith'},
-    {"id": 3, 'title': 'Flask and Jinja2', 'content': 'Jinja2 is powerful for templating.', 'author': 'Mike Lee'}
-] 
-
-@app.route('/posts') 
-def get_posts():
-    return render_template("posts.html", posts=posts)
-
-@app.route('/post/<int:id>') 
-def detail_post(id):
-    if id > 3:
-        abort(404)
-    post = posts[id-1]
-    return render_template("detail_post.html", post=post)
 
 @app.route('/resume')
 def resume():
     return render_template("resume.html")
+
